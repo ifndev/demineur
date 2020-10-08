bca930109c11b5e21fb34b3a050b779fca88d652
poslovitch <poslovitch@bentobox.world>
2020-10-08 22:36:28 +0200

    Applied: Function return type hints (PEP 484), PEP 008 recommendations

--- GameBoard.py
+++ GameBoard.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from Cell import Cell
+
 
 class GameBoard:
     def __init__(self, sizeX: int, sizeY: int):
@@ -7,16 +10,15 @@ class GameBoard:
 
         self.__grid = []
         
-        
         for y in range(sizeY):
             self.__grid.append([])
             for x in range(sizeX):
                 self.__grid[y].append(Cell())
 
-    def get_grid(self):
+    def get_grid(self) -> List[List[Cell]]:
         return self.__grid
 
-    def reveal(self, x: int, y: int):
+    def reveal(self, x: int, y: int) -> bool:
         self.__grid[x][y].set_hidden(False)
 
         if self.__grid[x][y].is_bomb():
@@ -25,13 +27,13 @@ class GameBoard:
         else:
             self.propagateFrom(x, y)
 
-    def toggle_flagged(self, x: int, y: int):
+    def toggle_flagged(self, x: int, y: int) -> None:
         self.__grid[x][y].set_flagged(not self.__grid[x][y].is_flagged())
 
-    def populate(self):
+    def populate(self) -> None:
         #TODO #5 implement populate()
         pass
 
-    def propagateFrom(self, x: int, y: int):
+    def propagateFrom(self, x: int, y: int) -> None:
         #TODO #1 implementGameBoard.propagate()
         pass
